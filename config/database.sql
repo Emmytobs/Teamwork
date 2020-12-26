7e5b4e98a8b6a2d0b5a154648ab444abd5f8957e
Emmytobs <eotobo11@gmail.com>
2020-12-26 11:49:02 +0100

    Added two tables articles and gifs

--- config/database.sql
+++ config/database.sql
@@ -16,3 +16,26 @@ CREATE TABLE Departments(
     department_id SERIAL NOT NULL PRIMARY KEY,
     name NOT NULL VARCHAR,
 );
+
+-- Articles Table
+CREATE TABLE articles(
+    article_id SERIAL NOT NULL PRIMARY KEY,
+    content TEXT NOT NULL,
+    created_by INT NOT NULL REFERENCES users(user_id),
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    in_department INT NOT NULL,
+    CONSTRAINT articles_departments_fkey
+        FOREIGN KEY (in_department)
+            REFERENCES departments(department_id)
+);
+
+-- Gifs Table
+CREATE TABLE gifs(
+    gif_id SERIAL NOT NULL PRIMARY KEY,
+    git_link VARCHAR NOT NULL,
+    created_by INT NOT NULL REFERENCES users(user_id),
+    in_department INT NOT NULL,
+    CONSTRAINT gifs_departments_fkey
+        FOREIGN KEY (in_department)
+            REFERENCES departments(department_id)
+);
