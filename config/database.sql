ce23b9fef74c7ea5105cd55d03ad9774fea56a08
Emmytobs <eotobo11@gmail.com>
2020-12-26 20:14:30 +0100

    Dropped the articles and gifs tables and added a posts table

--- config/database.sql
+++ config/database.sql
@@ -19,25 +19,38 @@ CREATE TABLE Departments(
     name NOT NULL VARCHAR,
 );
 
--- Articles Table
-CREATE TABLE articles(
-    article_id SERIAL NOT NULL PRIMARY KEY,
-    content TEXT NOT NULL,
+-- Posts Table
+CREATE TABLE posts(
+    post_id SERIAL NOT NULL PRIMARY KEY,
+    article TEXT,
+    gif_link VARCHAR,
     created_by INT NOT NULL REFERENCES users(user_id),
     created_at TIMESTAMPTZ DEFAULT NOW(),
     in_department INT NOT NULL,
-    CONSTRAINT articles_departments_fkey
+    CONSTRAINT posts_departments_fkey
         FOREIGN KEY (in_department)
             REFERENCES departments(department_id)
-);
+)
 
--- Gifs Table
-CREATE TABLE gifs(
-    gif_id SERIAL NOT NULL PRIMARY KEY,
-    git_link VARCHAR NOT NULL,
-    created_by INT NOT NULL REFERENCES users(user_id),
-    in_department INT NOT NULL,
-    CONSTRAINT gifs_departments_fkey
-        FOREIGN KEY (in_department)
-            REFERENCES departments(department_id)
-);
+-- -- Articles Table
+-- CREATE TABLE articles(
+--     article_id SERIAL NOT NULL PRIMARY KEY,
+--     content TEXT NOT NULL,
+--     created_by INT NOT NULL REFERENCES users(user_id),
+--     created_at TIMESTAMPTZ DEFAULT NOW(),
+--     in_department INT NOT NULL,
+--     CONSTRAINT articles_departments_fkey
+--         FOREIGN KEY (in_department)
+--             REFERENCES departments(department_id)
+-- );
+
+-- -- Gifs Table
+-- CREATE TABLE gifs(
+--     gif_id SERIAL NOT NULL PRIMARY KEY,
+--     git_link VARCHAR NOT NULL,
+--     created_by INT NOT NULL REFERENCES users(user_id),
+--     in_department INT NOT NULL,
+--     CONSTRAINT gifs_departments_fkey
+--         FOREIGN KEY (in_department)
+--             REFERENCES departments(department_id)
+-- );
