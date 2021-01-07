8efe748732fd4fbdcf76dbd6f65eb8cbd6636869
Emmytobs <eotobo11@gmail.com>
2021-01-07 22:20:37 +0100

    Imported the comment routes and registered it as a middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 const userRoutes = require('./routes/user.route');
 const postRoutes = require('./routes/post.route');
 const departmentRoutes = require('./routes/department.route');
+// const commentRoutes = require('./routes/comment.route');
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(userRoutes);
 app.use(postRoutes);
 app.use(departmentRoutes);
+// app.use(commentRoutes);
 
 app.get('/test', (req, res) => {
   res.send('Server returned a response!');
